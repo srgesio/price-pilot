df51fe439f197f33de71e1700a268b7113c34d30
srgesio <gesioiajm@gmail.com>
2025-09-06 16:00:07 -0300

    chore: pre-build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
